89efaf7f8cf3e78a3646ac5bd98a027293db3101
brodie02 <marshallbrodie0@gmail.com>
2023-02-14 21:23:01 +1030

    Add typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+import { gql } from 'apollo-server-express'
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    categories: [Category]
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    name: String
+    email: String
+    orders: [Order]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(name: String, email: String, password: String): User
+    updateProduct(_id: ID!, stock: Int!): Product
+    login(email: String!, password: String!): Auth
+  }
+`
