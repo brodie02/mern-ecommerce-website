6f573e60af7e6207f4a5276b475e335109041c0c
brodie02 <marshallbrodie0@gmail.com>
2023-02-14 20:58:18 +1030

    Add resolver mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -50,6 +50,56 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in')
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args)
+      const token = signToken(user)
+
+      return { token, user }
+    },
+    addOrder: async (parent, { products }, context) => {
+      if (context.user) {
+        const order = new Order({ products })
+
+        await User.findById(context.user._id, { $push: { orders: order } })
+
+        return order
+      }
+
+      throw new AuthenticationError('Not logged in')
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id), args, { new: true }
+      }
+
+      throw new AuthenticationError('Not logged in')
+    },
+    updateProduct: async (parent, { _id, stock }) => {
+      const decrement = stock - 1
+
+      return await Product.findByIdAndUpdate(_id, { $inc: { stock: decrement } }, { new: true })
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email})
+
+      if (!user) {
+        throw new AuthenticationError('Invalid email, please try again')
+      }
+
+      const correctPw = await user.isCorrectPassword(password)
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password, please try again')
+      }
+
+      const token = signToken(user)
+
+      return { token, user }
     }
   }
 }
+
+export default resolvers
