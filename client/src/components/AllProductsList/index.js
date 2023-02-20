29b16d55c547cc7728a268e8ba2b3c72bf570636
brodie02 <marshallbrodie0@gmail.com>
2023-02-20 21:12:03 +1030

    Add all products component

--- client/src/components/AllProductsList/index.js
+++ client/src/components/AllProductsList/index.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import { QUERY_ALL_PRODUCTS } from '../../utils/queries'
+
+import Product from '../Product'
 
 export default function AllProductList() {
+  const { loading, data } = useQuery(QUERY_ALL_PRODUCTS)
+
+  const products = data?.products || []
+
   return (
-    <div></div>
+    <div>
+      <h2>Products</h2>
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          products.map((item) => (
+            <Product 
+              key={item._id}
+              _id={item._id}
+              image={item.image}
+              name={item.name}
+              description={item.description}
+              quantity={item.quantity}
+              categories={item.categories}
+            />
+          ))
+        )}
+      </div>
+    </div>
   )
 }
