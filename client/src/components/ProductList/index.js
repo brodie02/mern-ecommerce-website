2f31b023e183e18d0d8a714bfff643f1873f1f20
brodie02 <marshallbrodie0@gmail.com>
2023-02-21 19:44:49 +1030

    Add product list styles

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Product from '../Product'
 import { useQuery } from '@apollo/client'
 import { QUERY_PRODUCTS } from '../../utils/queries'
+import './style.css'
 
 export default function ProductList({currentCategory}) {
   const { loading, data } = useQuery(QUERY_PRODUCTS, {
@@ -11,9 +12,9 @@ export default function ProductList({currentCategory}) {
   const products = data?.products || []
 
   return (
-    <div>
+    <div id='all-product-list'>
       <h2>Products</h2>
-      <div>
+      <div id='all-product-list-products'>
         {loading ? (
           <div>Loading...</div>
         ) : (
